179dfdbc098a21211d073b93d60ee47b55925a5c
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-12-11 10:41:08 +0100

    remove anyhow

--- examples/block1.rs
+++ examples/block1.rs
@@ -1,25 +1,31 @@
-use std::{io, process::Command};
-
-use anyhow::Context;
+use std::{
+    io::Write, // <--- bring flush() into scope
+    io::{self},
+    process::Command,
+};
 
 enum CommandResult {
     Success,
     Quit,
 }
 
-fn main_loop() -> anyhow::Result<CommandResult> {
+fn main_loop() -> Result<CommandResult, ()> {
+    print!("> ");
+    io::stdout().flush().unwrap();
     let mut line = String::new();
     io::stdin().read_line(&mut line).unwrap();
     let mut parts = line.split_whitespace();
-    let command = parts.next().context("No command given")?;
-    if command == "quit" {
-        return Ok(CommandResult::Quit);
+
+    // If no command was entered, execute nothing.
+    if let Some(command) = parts.next() {
+        if command == "quit" {
+            return Ok(CommandResult::Quit);
+        }
+        let args: Vec<&str> = parts.collect();
+        if let Err(err) = Command::new(command).args(args).spawn() {
+            eprintln!("{:?}", err);
+        }
     }
-    let args: Vec<&str> = parts.collect();
-    Command::new(command)
-        .args(args)
-        .spawn()
-        .with_context(|| format!("Failed to execute {}", command))?;
     Ok(CommandResult::Success)
 }
 
