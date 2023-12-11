169b4354bdbbbe4475262e56d38a9d96f08887e8
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-12-11 11:01:43 +0100

    create Cmd struct

--- examples/block1.rs
+++ examples/block1.rs
@@ -1,42 +1,53 @@
 use std::{
+    io,
     io::Write, // <--- bring flush() into scope
-    io::{self},
     process::Command,
 };
 
-enum CommandResult {
-    Success,
-    Quit,
+struct Cmd<'a> {
+    binary: &'a str,
+    args: Vec<&'a str>,
 }
 
-fn main_loop() -> Result<CommandResult, ()> {
-    print!("> ");
-    io::stdout().flush().unwrap();
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).unwrap();
-    let mut parts = line.split_whitespace();
+impl<'a> Cmd<'a> {
+    fn from_line(line: &'a str) -> Option<Self> {
+        let mut parts = line.split_whitespace();
+        parts.next().map(|binary| Cmd {
+            binary,
+            args: parts.collect(),
+        })
+    }
 
-    // If no command was entered, execute nothing.
-    if let Some(command) = parts.next() {
-        if command == "quit" {
-            return Ok(CommandResult::Quit);
-        }
-        let args: Vec<&str> = parts.collect();
-        if let Err(err) = Command::new(command).args(args).spawn() {
-            eprintln!("{:?}", err);
+    fn run(self) {
+        match Command::new(self.binary).args(self.args).spawn() {
+            Ok(mut child) => {
+                child.wait().expect("command wasn't running");
+            }
+            Err(e) => eprintln!("{:?}", e),
         }
     }
-    Ok(CommandResult::Success)
 }
 
 fn main() {
     loop {
-        match main_loop() {
-            Ok(CommandResult::Quit) => break,
-            Ok(CommandResult::Success) => {}
-            Err(e) => {
-                eprintln!("{:?}", e);
-            }
+        show_prompt();
+        let line = read_line();
+        if let Some(command) = Cmd::from_line(&line) {
+            command.run();
         }
     }
+}
+
+fn show_prompt() {
+    print!("> ");
+    // Flush stoud to ensure the prompt is displayed.
+    io::stdout().flush().expect("can't flush stdout");
+}
+
+fn read_line() -> String {
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("failed to read line from stdin");
+    line
 }
